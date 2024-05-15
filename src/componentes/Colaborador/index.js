c8e8fb161c9b35c26612f49ba2ca4aea48687c74
Vinicius Martins <vinibitmartins@gmail.com>
2024-05-15 19:29:47 -0300

    Aula 02: O que são componentes

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,10 @@
 import './colaborador.css'
+import { AiFillCloseCircle } from 'react-icons/ai' 
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+    const iconSize = 25
     return (<div className="colaborador">
+        <AiFillCloseCircle className="deletar" size={iconSize} onClick={aoDeletar} />            
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,16 +1,24 @@
 import Colaborador from '../Colaborador'
 import './time.css'
 
-const Time = ({ time, colaboradores }) => {
+const Time = ({ time, colaboradores, aoDeletar }) => {
     return (
 
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
-            <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
-            </div>
-        </section>
-
+        colaboradores.length > 0 && 
+            <section className='time' 
+                style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+                <h3 style={{ color: time.corSecundaria, borderColor: time.corSecundaria }}>{time.nome}</h3>
+                <div className='colaboradores'>
+                    {colaboradores.map((colaborador, indice) => 
+                        <Colaborador 
+                            key={indice} 
+                            colaborador={colaborador} 
+                            corDeFundo={time.corSecundaria}
+                            aoDeletar={aoDeletar}
+                        />
+                    )}
+                </div>
+            </section>
     )
 }
 
