67c1962f5f39d3aa924cf0a9234ad3484c56224d
Vincius Martins <vinibitmartins@gmail.com>
2024-05-27 12:13:55 -0300

    Aula 03: Como o React vê um componente

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,21 +1,28 @@
 import Colaborador from '../Colaborador'
 import './time.css'
+import hexToRgba from 'hex-to-rgba'
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
-
         colaboradores.length > 0 && 
             <section className='time' 
-                style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-                <h3 style={{ color: time.corSecundaria, borderColor: time.corSecundaria }}>{time.nome}</h3>
+                style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
+                <div className='input-cor'>
+                    <label>Cor do time: </label>
+                    <input type='color'                         
+                        value={time.cor} 
+                        onChange={event => mudarCor(event.target.value, time.nome)} />
+                </div>
+                <h3 style={{ color: time.corSecundaria, borderColor: time.cor }}>{time.nome}</h3>
                 <div className='colaboradores'>
-                    {colaboradores.map((colaborador, indice) => 
-                        <Colaborador 
-                            key={indice} 
-                            colaborador={colaborador} 
-                            corDeFundo={time.corSecundaria}
-                            aoDeletar={aoDeletar}
-                        />
+                    {
+                        colaboradores.map((colaborador, indice) => 
+                            <Colaborador 
+                                key={indice} 
+                                colaborador={colaborador} 
+                                corDeFundo={time.cor}
+                                aoDeletar={aoDeletar}
+                            />
                     )}
                 </div>
             </section>
